1ddb5b61cae239317da16b8ddb2e04b7406e5658
Vasiliki Kokora <vkokora@taxiplon.gr>
2024-06-04 15:17:49 +0300

    Change table

--- index.js
+++ index.js
@@ -604,7 +604,7 @@ app.get('/rentacarPhotos', compression(), (req, res) => {
 
 
 app.post('/rentacarPhotos', compression(), (req, res) => {
-  const { rncSupplierId, numberOfRows, inputsFromFirstColumn } = req.query;
+  const { rncSupplierId, numberOfRows, inputsFromFirstColumn, carModels } = req.query;
 
   // Define the uploadFields middleware function inside the route handler
   const uploadFields = (req, res, next) => {
@@ -663,8 +663,8 @@ app.post('/rentacarPhotos', compression(), (req, res) => {
 
           // Insert data into the database
           await client.query(
-            `INSERT INTO rnc_photos (rnc_supplier_id, photo1, photo2, photo3) VALUES ($1, $2, $3, $4)`,
-            [rncSupplierId, photo1, photo2, photo3]
+            `INSERT INTO rnc_photos (model,  photo1, photo2, photo3, rnc_supplier_id) VALUES ($1, $2, $3, $4, $5)`,
+            [carModels[i], photo1, photo2, photo3, rncSupplierId]
           );
         }
 
